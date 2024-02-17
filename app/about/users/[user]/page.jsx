d68f0194fcd83dd0bfb253833943dde74b1b70c0
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-17 11:49:44 +0530

    Redirect in NEXt

--- app/about/users/[user]/page.jsx
+++ app/about/users/[user]/page.jsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { redirect } from "next/navigation";
+
 const User = ({ params }) => {
+  if (params.user == "chirag") {
+    redirect("/login");
+  }
   return (
     <span className="text-red-600 text-5xl font-bold">
       Hello {params.user}!
