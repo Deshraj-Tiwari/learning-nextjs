24f089b3d0bdd4b82a1f68529aff95d751c25aac
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-17 09:03:56 +0530

    Ignoring Routes using ()

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <span className="text-red-600 text-5xl font-bold">Login</span>;
+};
+
+export default Login;
---
+++ app/(auth)/register/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <span className="text-red-600 text-5xl font-bold">Register</span>;
+};
+
+export default Register;
