7777d26762fc5185d067b6eb606d6b02e38c0a28
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-17 13:03:02 +0530

    Images in NEXT

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,9 @@
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
+import Image from "next/image";
+import myImage from "../public/image.jpg";
+
 export default function Home() {
   const router = useRouter();
   const [pageTo, setPageTo] = useState("");
@@ -12,7 +15,6 @@ export default function Home() {
     router.push(pageTo);
   }
 
-  // ? You also have another desktop opened for CBSE STUFF. Check it out as well
   return (
     <section>
       <span className="text-red-600 text-5xl font-bold">Home</span>
@@ -34,6 +36,22 @@ export default function Home() {
           Redirect
         </button>
       </form>
+      {/* //- Using NEXT js image tag will compress and optimise your image */}
+      <Image
+        className="inline"
+        src={myImage}
+        alt="Image of teamwork"
+        style={{ width: "25rem", height: "auto" }}
+      />
+      {/* //- To use an image with the help of an external link, you have to provide the domain of the image provider in the nex.config.mjs file by creating an images object and then providing an array as value for they key of domains, which should be including the domain of the provider. */}
+      <Image
+        className="inline"
+        src="https://images.unsplash.com/photo-1707762745645-45bf0b25eb7d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Image of lighthouse"
+        width={200}
+        height={200}
+      />
     </section>
   );
+  // ? You also have another desktop opened for CBSE STUFF. Check it out as well
 }
