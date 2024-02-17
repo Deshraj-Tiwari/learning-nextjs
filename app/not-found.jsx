6f9a760b53810df4a92824fb7403d76b0c7e51a4
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-17 11:42:07 +0530

    Page Not Found page in NEXT

---
+++ app/not-found.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <span className="text-green-600 text-3xl font-bold">
+      Create your own PageNotFound page. <br />
+      <br />
+      Always remember to keep the not found file directly in the app directory
+      and name it not-found
+    </span>
+  );
+};
+
+export default NotFound;
