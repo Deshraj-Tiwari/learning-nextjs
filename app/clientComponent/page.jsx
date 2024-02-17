36bb1dc49b11801510e3eaf29b1242860aaa5ae0
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-02-17 12:08:21 +0530

    Client and Server components

---
+++ app/clientComponent/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+const clientComponent = () => {
+  return (
+    <span className="text-purple-600 text-3xl font-bold">
+      Use client component by writing "use client". Use for making frontend
+      interactive like using onclick, onsubmit, usestate, useeffect ... etc.
+    </span>
+  );
+};
+
+export default clientComponent;
---
+++ app/serverComponent/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const serverComponent = () => {
+  return (
+    <span className="text-purple-600 text-3xl font-bold">
+      Any component is server component by default in NEXT js and you can use it
+      to do sever side rendering, Static site generation and stuff like that.
+    </span>
+  );
+};
+
+export default serverComponent;
