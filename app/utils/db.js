4f2a25c615b5ff92fc77764af27dceef957b4a4b
Deshraj Tiwari <158583956+Deshraj-Tiwari@users.noreply.github.com>
2024-02-18 08:23:57 +0000

    Getting started with backend and learning about GET

---
+++ app/utils/db.js
@@ -0,0 +1,32 @@
+export const users = [
+    {
+        id: "1",
+        name: "John Doe",
+        email: "JohnDoe1@gmail.com",
+        password: "123"
+    },
+    {
+        id: "2",
+        name: "Donald Trump",
+        email: "DonaldTrump2@gmail.com",
+        password: "234"
+    },
+    {
+        id: "3",
+        name: "Barack Obama",
+        email: "BarackObama3@gmail.com",
+        password: "345"
+    },
+    {
+        id: "4",
+        name: "Elon Musk",
+        email: "Elon Musk4@gmail.com",
+        password: "456"
+    }, 
+    {
+        id: "5",
+        name: "Leonel Messi",
+        email: "LeonelMessi5@gmail.com",
+        password: "567"
+    }
+]
